f4ed952022153a163612c9e37e8ec59ceb0da890
Francis Serpa <fcserpa@gmail.com>
2015-10-20 19:10:47 -0700

    last details on app and README

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# firebase_demo
+# Firebase
+
+Firebase is a service that provides data storage, user authentication, static hosting for your apps.  Data is stored as JSON objects, and users are updated in real time when data changes or is added.  
+
+Firebase has a couple of short and easy tutorials on their site for making messaging apps and trying out the service, one for vanilla Firebase-
+
+https://www.firebase.com/tutorial/#gettingstarted
+
+and one for AngularFire (Firebase with Angular)-
+
+https://www.firebase.com/tutorial/#tutorial/angular/0
+
+They'll show you the basics for getting started.  (For ease, the tutorials put all of the JS in the HTML, so for a real app you probably want to put it in its own file for separation of concerns issues.)  There are several other good sample projects on the firebase website, like Tetris!
+
+https://www.firebase.com/tutorial/#session/xy1706rxz1z
+
+To use Firebase in your app, you can include this in your HTML-
+<script src="https://cdn.firebase.com/js/client/2.3.1/firebase.js"></script>
+or
+$ npm install firebase --save
+
+Likewise for AngularFire use-
+<script src="https://cdn.firebase.com/libs/angularfire/1.1.3/angularfire.min.js"></script>
+or
+$ npm install angularfire --save
+
+Here are some helpful links-
 Firebase site-
 https://www.firebase.com/
 
--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-app = angular.module('movieApp', []);
+app = angular.module('movieApp', ['firebase']);
 
-app.controller("MovieCtrl", function($scope){
-	$scope.movie = {
-		title: "Star Wars VII: The Force Awakens",
-		theater: "Alamo Drafthouse",
-		date: "12/17/2015",
-		time: "10:30pm"
+app.constant('FIREBASE_URI', 'https://blistering-fire-5819.firebaseio.com/demo/');
+
+app.controller("MovieCtrl", ['$scope', 'MoviesService', function($scope, MoviesService){
+	$scope.newMovie = {title: '', theater: '', date: '', time: ''}
+	$scope.movies = MoviesService.getMovies();
+
+	$scope.addMovie = function() {
+		MoviesService.addMovie(angular.copy($scope.newMovie));
+		$scope.newMovie = { title: '', theater: '', date: '', time: ''};
+	};
+
+	$scope.updateMovie = function(id) {
+		MoviesService.updateMovie(id);
+	};
+
+	$scope.removeItem = function(item) {
+		ItemService.removeItem(item);
 	}
+}]);
 
-});
+app.factory('MoviesService', ['FIREBASE_URI', '$firebaseArray', function (FIREBASE_URI, $firebaseArray){
+	var ref = new Firebase(FIREBASE_URI);
+	var movies = $firebaseArray(ref);
+
+	var getMovies = function() {
+		return movies;
+	};
+
+	var addMovie = function(movie) {
+		movies.$add(movie);
+	};
+
+	var updateMovie = function (id) {
+		movies.$save(id);
+	};
+
+	var removeMovie = function (id) {
+		movies.$remove(id);
+	};
+
+	return {
+		getMovies: getMovies,
+		addMovie: addMovie,
+		updateMovie: updateMovie,
+		removeMovie: removeMovie
+	}
+}]);
